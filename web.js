8de056b6ebe6aae549f0c69cda6560afc3748b0d
Bill Wright <billwinvt@gmail.com>
2013-07-09 22:47:54 +0000

    Update web.js

--- web.js
+++ web.js
@@ -3,10 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var message=fs.readFileSync("index.html");    
+    response.send(message.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
